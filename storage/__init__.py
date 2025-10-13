39f365784032faab3663ff2a0e22f8cd19e049af
bckl3j5la866 <bckl3j5la866@mail.ru>
2025-10-13 17:11:02 +0900

    Add files via upload

---
+++ storage/__init__.py
@@ -0,0 +1,11 @@
+from .storage import (
+    mark_as_notified,
+    filter_by_boundary,
+    update_boundary,
+    get_boundary_date,
+    load_boundary,
+    save_boundary,
+    load_notified,
+    save_notified,
+    should_initialize_boundary
+)
